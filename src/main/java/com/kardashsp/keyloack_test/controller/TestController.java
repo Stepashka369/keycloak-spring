b16ef3b85ad0671f4019ba1ca4b9a48b2ccbb283
Kardash Stepan <stsiapan.kardash@softclub.by>
2024-07-19 15:23:52 +0300

    1/ Pom ready

---
+++ src/main/java/com/kardashsp/keyloack_test/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.kardashsp.keyloack_test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+
+
+}
