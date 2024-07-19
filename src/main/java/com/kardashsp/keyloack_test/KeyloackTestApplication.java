b926fdf63eefcb25b7d803e80641caf404bcd5db
Kardash Stepan <stsiapan.kardash@softclub.by>
2024-07-19 09:45:33 +0300

    1. Gitignore added.

---
+++ src/main/java/com/kardashsp/keyloack_test/KeyloackTestApplication.java
@@ -0,0 +1,13 @@
+package com.kardashsp.keyloack_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyloackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyloackTestApplication.class, args);
+	}
+
+}
